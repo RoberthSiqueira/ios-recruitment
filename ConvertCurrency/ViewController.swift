46d7d373f0e4f898412ff9cdb3820405250299a8
Roberth Siqueira <roberth.siqueira@zup.com.br>
2019-08-30 11:41:18 -0300

    Initial Commit

---
+++ ConvertCurrency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConvertCurrency
+//
+//  Created by Roberth Henrique Siqueira on 30/08/19.
+//  Copyright © 2019 Roberth Siqueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ ConvertCurrencyTests/ConvertCurrencyTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConvertCurrencyTests.swift
+//  ConvertCurrencyTests
+//
+//  Created by Roberth Henrique Siqueira on 30/08/19.
+//  Copyright © 2019 Roberth Siqueira. All rights reserved.
+//
+
+import XCTest
+@testable import ConvertCurrency
+
+class ConvertCurrencyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
